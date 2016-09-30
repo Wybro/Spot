922a84c4969a2fbee7890767cc7f83a9b1140f15
Connor Wybranowski <connor@wybro.com>
2016-09-30 15:17:55 -0400

    Location Tracking -MapKit integration to obtain current user location

--- Spot/ViewController.swift
+++ Spot/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,26 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Location:", location)
+            let span = MKCoordinateSpanMake(0.002, 0.002)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed with error:", error)
+    }
 }
 
