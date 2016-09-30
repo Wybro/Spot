7203dbab9cf96e38b48257be7108b1e801509d3d
Connor Wybranowski <connor@wybro.com>
2016-09-30 13:01:24 -0400

    Initial Commit

---
+++ Spot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spot
+//
+//  Created by Connor Wybranowski on 9/30/16.
+//  Copyright © 2016 com.Wybro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
